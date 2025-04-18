930160b0c53f66c0a2255587278ad70bbfc6c2fe
Shion <seoy1108@gmail.com>
2025-04-19 05:25:52 +0900

    feat: 전역 스타일 객체 생성

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import { RecoilRoot } from 'recoil';
 
 import App from './App.tsx';
 import { queryClient } from './hooks/queryClient.ts';
+import { GlobalStyles } from './styles/GlobalStyles.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
+        <GlobalStyles />
         <App />
       </RecoilRoot>
     </QueryClientProvider>
