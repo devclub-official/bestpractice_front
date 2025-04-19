f258c4c92b1635aaa03949ca305a717c01fc4257
Shion <seoy1108@gmail.com>
2025-04-19 09:15:19 +0900

    style: 스피너 추가 및 코드 스타일 수정

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -6,11 +6,12 @@ import { FILE_FORMATS, FRAMEWORKS, FUNCTION_LABELS, FUNCTION_OPTIONS, LANGUAGES 
 
 import * as Styled from './SearchForm.styles';
 import { useEffect, useState } from 'react';
+import Spinner from './Spinner';
 
 const SearchForm = () => {
-  const { mutate: search, data } = useSearchMutation();
-  const [isSearched, setIsSearched] = useState(false)
-console.log('data', data)
+  const { mutate: search, data, isPending } = useSearchMutation();
+  const [isSearched, setIsSearched] = useState(false);
+
   const {
     radios: { language, framework, fileFormat },
     selectRadios,
@@ -26,74 +27,90 @@ console.log('data', data)
       selected_options: selectedOptions,
     });
   };
-  useEffect(()=>{
-    if(data){
-      setIsSearched(true)
+
+  useEffect(() => {
+    if (data) {
+      setIsSearched(true);
     }
-  },[data])
+  }, [data]);
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
   return (
     <Styled.Wrapper>
-      {!isSearched && <Styled.Form>
-        <LabelInput label="언어">
-          <div style={{ display: 'flex', gap: '1.2rem' }}>
-            {LANGUAGES.map((lang) => (
-              <Styled.InputWrapper key={lang}>
-                <input type="radio" name="language" value={lang} checked={language === lang} onChange={selectRadios} />
-                <span>{lang}</span>
-              </Styled.InputWrapper>
-            ))}
-          </div>
-        </LabelInput>
+      {!isSearched && (
+        <Styled.Form>
+          <LabelInput label="언어">
+            <div style={{ display: 'flex', gap: '1.2rem' }}>
+              {LANGUAGES.map((lang) => (
+                <Styled.InputWrapper key={lang}>
+                  <input
+                    type="radio"
+                    name="language"
+                    value={lang}
+                    checked={language === lang}
+                    onChange={selectRadios}
+                  />
+                  <span>{lang}</span>
+                </Styled.InputWrapper>
+              ))}
+            </div>
+          </LabelInput>
 
-        <LabelInput label="프레임워크">
-          <div style={{ display: 'flex', gap: '1.2rem' }}>
-            {FRAMEWORKS.map((fw) => (
-              <Styled.InputWrapper key={fw}>
-                <input type="radio" name="framework" value={fw} checked={framework === fw} onChange={selectRadios} />
-                <span>{fw}</span>
-              </Styled.InputWrapper>
-            ))}
-          </div>
-        </LabelInput>
+          <LabelInput label="프레임워크">
+            <div style={{ display: 'flex', gap: '1.2rem' }}>
+              {FRAMEWORKS.map((fw) => (
+                <Styled.InputWrapper key={fw}>
+                  <input type="radio" name="framework" value={fw} checked={framework === fw} onChange={selectRadios} />
+                  <span>{fw}</span>
+                </Styled.InputWrapper>
+              ))}
+            </div>
+          </LabelInput>
 
-        <LabelInput label="파일 포맷">
-          <div style={{ display: 'flex', gap: '1.2rem' }}>
-            {FILE_FORMATS.map((format) => (
-              <Styled.InputWrapper key={format}>
-                <input
-                  type="radio"
-                  name="fileFormat"
-                  value={'json'}
-                  // checked={fileFormat === format}
-                  onChange={selectRadios}
-                />
-                <span>{format}</span>
-              </Styled.InputWrapper>
-            ))}
-          </div>
-        </LabelInput>
-        <LabelInput label="기능">
-          <div style={{ width: '100%', textAlign: 'start' }}>
-            {Object.entries(FUNCTION_OPTIONS).map(([key, options]) => (
-              <div key={key}>
-                <h4>{FUNCTION_LABELS[key] ?? key}</h4> {/* key에 대응하는 한글이 없으면 key 그대로 사용 */}
-                {options.map((option) => (
-                  <label key={option}>
-                    <input type="radio" name={key} value={option} onChange={() => selectOptions(key, option)} />
-                    {option}
-                  </label>
-                ))}
-              </div>
-            ))}
-          </div>
-        </LabelInput>
-      </Styled.Form>}
-      {isSearched && <Styled.ScreenContainer>
+          <LabelInput label="파일 포맷">
+            <div style={{ display: 'flex', gap: '1.2rem' }}>
+              {FILE_FORMATS.map((format) => (
+                <Styled.InputWrapper key={format}>
+                  <input
+                    type="radio"
+                    name="fileFormat"
+                    value={'json'}
+                    // checked={fileFormat === format}
+                    onChange={selectRadios}
+                  />
+                  <span>{format}</span>
+                </Styled.InputWrapper>
+              ))}
+            </div>
+          </LabelInput>
+          <LabelInput label="기능">
+            <div style={{ width: '100%', textAlign: 'start' }}>
+              {Object.entries(FUNCTION_OPTIONS).map(([key, options]) => (
+                <div key={key}>
+                  <h4>{FUNCTION_LABELS[key] ?? key}</h4> {/* key에 대응하는 한글이 없으면 key 그대로 사용 */}
+                  {options.map((option) => (
+                    <label key={option}>
+                      <input type="radio" name={key} value={option} onChange={() => selectOptions(key, option)} />
+                      {option}
+                    </label>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </LabelInput>
+        </Styled.Form>
+      )}
+      {isSearched && (
+        <Styled.ScreenContainer>
           {/* data?.data?.content.replace(r'\\u[0-9a-fA-F]{4}', '') */}
           <pre>{JSON.stringify(data?.data?.content.replace('\\u[0-9a-fA-F]{4}', ''), null, 2)}</pre>
-        </Styled.ScreenContainer>}
+        </Styled.ScreenContainer>
+      )}
       {!isSearched && <Styled.StyledButton onClick={handleSubmit}>생성</Styled.StyledButton>}
-        {isSearched &&<Styled.StyledButton onClick={()=>setIsSearched(!isSearched)}>다시하기</Styled.StyledButton> }
+      {isSearched && <Styled.StyledButton onClick={() => setIsSearched(!isSearched)}>다시하기</Styled.StyledButton>}
     </Styled.Wrapper>
   );
 };
